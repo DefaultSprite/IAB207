b748bae6083780cc62932b079d85450240605c87
michyhems <64618905+michyhems@users.noreply.github.com>
2023-10-23 10:09:01 +1000

    Update models.py

--- server/models.py
+++ server/models.py
@@ -15,30 +15,37 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String(255), nullable=False)
     # relation to call user.comments and comment.created_by
     comments = db.relationship('Comment', backref='user')
+    created_event = db.relationship('Event', backref='user')
+    orders = db.relationship('TicketOrder', backref='user')
     
     # string print method
     def __repr__(self):
         return f"Name: {self.name}"
 
-class Events(db.Model):
-    __tablename__ = 'Events'
+class Event(db.Model):
+    __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
-    userID = db.Column(db.Integer, db.ForeignKey('users.id'))
+    creator_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     name = db.Column(db.String(80))
+    description = db.Column(db.String(200))
+    image = db.Column(db.String(400))
     venue_name = db.Column(db.String(80))
     address = db.Column(db.String(80))
     artist = db.Column(db.String(80))
     date_time = db.Column(db.DateTime)
-    description = db.Column(db.String(200))
+
     image = db.Column(db.String(400))
     maxSeating = db.Column(db.Integer)
     currentSeating = db.Column(db.Integer)
-    comments = db.relationship('Comment', backref='Events')
-    status = db.relationship('Status', backref='Events')
+    # ... Create the Comments db.relationship
+	# relation to call destination.comments and comment.destination
+    comments = db.relationship('Comment', backref='event')
+    orders = db.relationship('TicketOrder', backref='event')
+    stat = db.relationship('EventStatus', backref='event')
+	
     # string print method
     def __repr__(self):
         return f"Name: {self.name}"
-
 
 class Comment(db.Model):
     __tablename__ = 'comments'
@@ -47,32 +54,32 @@ class Comment(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.now())
     # add the foreign key
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    destination_id = db.Column(db.Integer, db.ForeignKey('Events.id'))
+    destination_id = db.Column(db.Integer, db.ForeignKey('events.id'))
 
     # string print method
     def __repr__(self):
         return f"Comment: {self.text}"
+    
+class TicketOrder(db.Model):
+    __tablename__= 'Order'
+    id = db.Column(db.Integer, primary_key=True)
+    ticketNum = db.Column(db.Integer)
+    Event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    def __repr__(self):
+        return f"Name: {self.ticketNum}"
 
 class Status(enum.Enum):
     a = 'Active'
     c = 'Cancelled'
     e = 'Expired'
 
-
 class EventStatus(db.Model):
-    __tablename__ = 'EventStatus'
+    __tablename__ = 'event_status'
     id = db.Column(db.Integer, primary_key=True)
-    Event_id = db.Column(db.Integer, db.ForeignKey('Events.id'))
+    Event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
     status = db.Column(Enum(Status))
 
     def __repr__(self):
         return f"Name: {self.status}"
 
-class TicketOrder(db.Model):
-    __tablename__= 'Order'
-    id = db.Column(db.Integer, primary_key=True)
-    ticketNum = db.Column(db.Integer)
-    Event_id = db.Column(db.Integer, db.ForeignKey('Events.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    def __repr__(self):
-        return f"Name: {self.ticketNum}"
