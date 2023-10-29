e8c01afa571bf0f99c0a068577a4f7920f1db3ed
DirtHut <n11597861@qut.edu.au>
2023-10-30 09:09:00 +1000

    added docstrings to created_database

--- created_database.py
+++ created_database.py
@@ -1,6 +1,15 @@
 from server import db, create_app
-app = create_app()
-ctx = app.app_context()
-ctx.push()
-db.create_all()
-quit()
+
+def create_database():
+    """
+    This function creates a database. It firstly creates an App and assigns it's
+    AppContent to ctx and pushes that AppContext to the current context so that
+    when the database is created it can use that context.
+    """
+    app = create_app()
+    ctx = app.app_context()
+    ctx.push()
+    db.create_all()
+    quit() # It is possible that this is redundant
+
+create_database()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from server import create_app
 
+# creates main
 if __name__ == '__main__':
     app = create_app()
     app.run(debug = True)
