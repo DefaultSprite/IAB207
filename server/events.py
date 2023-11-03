673af2465148efdac8825bc770196edbbbe846eb
Hemble <michyhems@gmail.com>
2023-11-03 10:50:31 +1000

    import update

--- server/events.py
+++ server/events.py
@@ -6,7 +6,7 @@ from . import db
 import os
 from werkzeug.utils import secure_filename
 from flask_login import login_required, current_user
-from datetime import date, datetime, time
+from datetime import date, datetime
 
 event_bp = Blueprint('event', __name__, url_prefix='/events')
 
